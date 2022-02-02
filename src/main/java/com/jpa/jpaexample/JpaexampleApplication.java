8e0ef75f13806bf76fae0b2f6769458a251bb208
Shubham <sb.skyfall@gmail.com>
2022-02-02 13:08:41 +0530

    hotfix new commit

--- src/main/java/com/jpa/jpaexample/JpaexampleApplication.java
+++ src/main/java/com/jpa/jpaexample/JpaexampleApplication.java
@@ -1,6 +1,6 @@
 package com.jpa.jpaexample;
 
-import com.jpa.jpaexample.strategy.SearchStrategy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
